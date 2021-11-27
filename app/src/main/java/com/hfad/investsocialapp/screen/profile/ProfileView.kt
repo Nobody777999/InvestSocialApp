9f75949a56894485c92d26199356d15be0afb0e6
Anastasia Konovalova <pochtaplay2013@gmail.com>
2021-11-27 15:37:44 +0300

    Profile Card Edit

--- app/src/main/java/com/hfad/investsocialapp/screen/profile/ProfileView.kt
+++ app/src/main/java/com/hfad/investsocialapp/screen/profile/ProfileView.kt
@@ -1,23 +1,28 @@
 package com.hfad.investsocialapp.screen.profile
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.hfad.investsocialapp.R
 
 @ExperimentalComposeUiApi
 @Composable
-fun ProfileView(navController: NavController, homeViewModel: ProfileViewModel) {
+fun ProfileView(navController: NavController, ProfileViewModelViewModel: ProfileViewModel) {
 
     Box(
         modifier = Modifier
@@ -25,15 +30,129 @@ fun ProfileView(navController: NavController, homeViewModel: ProfileViewModel) {
     ) {
 
 
-        Row() {
-            Image(painterResource(R.drawable.photo), contentDescription = "photo")
-            Column() {
-                Text(text = "Name")
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "Subscribe")
-                }
 
+
+
+
+
+
+    }
+
+}
+@Composable
+fun ColumnItem() {
+    Column(
+        modifier = Modifier
+
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .height(30.dp)
+            .background(Color.White)
+            .border(1.dp, Color.Blue),
+        verticalArrangement = Arrangement.Top,
+
+
+        ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White)
+
+        ) {
+            Column(horizontalAlignment = Alignment.Start)
+            {
+                Image(
+                    painterResource(R.drawable.photo),
+                    contentDescription = "profile_photo",
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .width(60.dp)
+                        .height(60.dp)
+                        .clip(CircleShape)
+                )
+            }
+
+            Column() {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    text = "Name",
+                    fontSize = 30.sp,
+                )
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    fontSize = 20.sp,
+                    text = "Rating"
+                )
+                Row(horizontalArrangement = Arrangement.End) {
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "Подписаться")
+                    }
+                }
             }
         }
+        Row(
+            modifier = Modifier
+                .background(Color.Red)
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+
+postStructure()
+        }
+
     }
+}
+
+//функция должна принимать имя и рейтинг для отображения fun postStructure(String Name, Int Rating){
+@Composable
+fun postStructure() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+
+    ) {
+        Column(horizontalAlignment = Alignment.Start)
+        {
+            Image(
+                painterResource(R.drawable.photo),
+                contentDescription = "profile_photo",
+                modifier = Modifier
+                    .padding(8.dp)
+                    .width(30.dp)
+                    .height(30.dp)
+                    .clip(CircleShape)
+            )
+        }
+        Spacer(modifier = Modifier.padding(40.dp))
+        Column() {
+            Row() {
+                Text(
+                    modifier = Modifier
+
+                        .padding(8.dp),
+                    text = "Name",
+                    fontSize = 20.sp,
+                )
+                Text(
+                    modifier = Modifier
+
+                        .padding(8.dp),
+                    fontSize = 15.sp,
+                    text = "Rating"
+                )
+            }
+
+
+        }
+    }
+}
+@Preview
+@Composable
+fun showColumnItem(){
+    ColumnItem()
 }
