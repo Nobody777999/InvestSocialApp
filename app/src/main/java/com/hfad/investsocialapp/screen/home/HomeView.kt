32f8980ddda5bbfd4fc06273b7e0df643deb9cbf
Anastasia Konovalova <pochtaplay2013@gmail.com>
2021-11-27 15:14:02 +0300

    Add Profile card

--- app/src/main/java/com/hfad/investsocialapp/screen/home/HomeView.kt
+++ app/src/main/java/com/hfad/investsocialapp/screen/home/HomeView.kt
@@ -1,7 +1,10 @@
 package com.hfad.investsocialapp.screen.home
-
+import com.hfad.investsocialapp.R
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.DragInteraction
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -11,8 +14,13 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
 
 @Composable
@@ -55,17 +63,105 @@ fun HomeView(navController: NavController, homeViewModel: HomeViewModel) {
 fun ColumnItem() {
     Column(
         modifier = Modifier
+
             .fillMaxWidth()
+            .fillMaxHeight()
             .height(30.dp)
             .background(Color.White)
             .border(1.dp, Color.Blue),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        verticalArrangement = Arrangement.Top,
+
 
     ) {
-        Text(text = "Здесь будет пост", color = Color.Blue)
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+
+        ){
+            Column(horizontalAlignment = Alignment.Start)
+                 {
+                Image(
+                    painterResource(R.drawable.photo),
+                    contentDescription = "profile_photo",
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .width(60.dp)
+                        .height(60.dp)
+                        .clip(CircleShape)
+                )
+            }
+
+            Column() {
+                Text( modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                    text = "Name",
+                    fontSize = 30.sp,)
+                Text(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                    fontSize = 20.sp,
+                    text = "Rating")
+                Row(horizontalArrangement = Arrangement.End) {
+                    Button(onClick = { /*TODO*/ }){
+                        Text(text = "Подписаться")
+                    }
+                }
+            }
+        }
+Row (modifier = Modifier
+    .background(Color.Red)
+    .fillMaxWidth()
+    .fillMaxHeight()
+) {
+
+    postStructure()
+    
+}
+
     }
 }
+//функция должна принимать имя и рейтинг для отображения fun postStructure(String Name, Int Rating){
+@Composable
+fun postStructure(){
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .background(Color.White)
+
+    ){
+        Column(horizontalAlignment = Alignment.Start)
+        {
+            Image(
+                painterResource(R.drawable.photo),
+                contentDescription = "profile_photo",
+                modifier = Modifier
+                    .padding(8.dp)
+                    .width(30.dp)
+                    .height(30.dp)
+                    .clip(CircleShape)
+            )
+        }
+        Spacer(modifier = Modifier.padding(40.dp))
+        Column() {
+            Row() {
+                Text( modifier = Modifier
+
+                    .padding(8.dp),
+                    text = "Name",
+                    fontSize = 20.sp,)
+                Text(modifier = Modifier
+
+                    .padding(8.dp),
+                    fontSize = 15.sp,
+                    text = "Rating")
+            }
+
+
+            }
+        }
+    }
+
+
 
 @Preview
 @Composable
